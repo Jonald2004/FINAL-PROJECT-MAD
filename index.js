9e23c3c63d237a291c115413b3e0d5eb8cfbaee9
Jonald2004 <filemonjonald31@gmail.com>
2025-04-25 11:47:12 +0800

    Jonald: pemeriksaan

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import Pages1 from './Pages1';
 import tes2 from './tes2';
+import Exercise8 from './Exercise8';
 
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => tes2);
+AppRegistry.registerComponent(appName, () => Exercise8);
